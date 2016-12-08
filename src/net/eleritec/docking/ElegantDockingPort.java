ebb064a5d8828dd17ab3b8ef7e71476b6c08bfe9
dmitrykolesnikovich <kolesnikovichdn@gmail.com>
2016-12-08 04:25:16 +0300

    initial commit

---
+++ src/net/eleritec/docking/ElegantDockingPort.java
@@ -0,0 +1,13 @@
+package net.eleritec.docking;
+
+
+public class ElegantDockingPort extends DefaultDockingPort {
+	public ElegantDockingPort() {
+		setComponentProvider(new ChildComponentDelegate());
+		setBorderManager(new StandardBorderManager(new ShadowBorder()));
+	}
+	
+	public void add(ElegantPanel view) {
+		dock(view.getDockable(), CENTER_REGION); 
+	}
+}
